18e278e3ef71fedd035021b211032bd809b0db0a
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-22 10:18:05 -1000

    Update getSP500Tickers.py

--- getSP500Tickers.py
+++ getSP500Tickers.py
@@ -22,29 +22,4 @@ def save_sp500_tickers():
         
     return tickers
 
-#save_sp500_tickers()
-
-
-def get_data_from_morningstar(reload_sp500=False):
-    
-    if reload_sp500:
-        tickers = save_sp500_tickers()
-    else:
-        with open("sp500tickers.pickle","rb") as f:
-            tickers = pickle.load(f)
-    
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
-
-    start = dt.datetime(2000, 1, 1)
-    end = dt.datetime(2018, 1, 1)
-    
-    for ticker in tickers:
-        # just in case your connection breaks, we'd like to save our progress!
-        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, "morningstar", start, end)
-            df.to_csv('stock_dfs/{}.csv'.format(ticker))
-        else:
-            print('Already have {}'.format(ticker))
-
-get_data_from_morningstar()
+save_sp500_tickers()
