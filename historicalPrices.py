116e72f33ff2c6a11efb7d7866880a7c9b618093
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-22 10:17:01 -1000

    Update historicalPrices.py

--- historicalPrices.py
+++ historicalPrices.py
@@ -1,16 +1,27 @@
+import csv
+import os
 import datetime as dt
-import pandas as pd
 import pandas_datareader.data as web
 
-start = dt.datetime(2010,1,1)
-end = dt.datetime(2018,1,1)
+#create source folder if it doesnt exist yet
+if not os.path.exists('stock_dfs'):
+    os.makedirs('stock_dfs')
 
-#use 'stooq' for indexes no dates necessary
-#df = web.DataReader('^DJI', 'stooq')
-#use 'morningstar' for stocks
+#open csv
+with open("sandp500.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
 
-df = web.DataReader('AAPL', 'morningstar', start, end)
-
-
-#print(df.head())
-df.to_csv('AAPL.csv')
+    for row in reader:
+        ticker = (row['ticker'])
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            start = dt.datetime(2010,1,1)
+            end = dt.datetime(2018,1,1)
+            #use 'morningstar' for stocks
+            df = web.DataReader('AAPL', 'morningstar', start, end)
+            #use 'stooq' for indexes no dates necessary
+            #df = web.DataReader('^DJI', 'stooq')
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            #you can also print these to test the program instead of going head first into csv
+            #print(df.head())
+        else:
+            print('Already have {}'.format(ticker))
